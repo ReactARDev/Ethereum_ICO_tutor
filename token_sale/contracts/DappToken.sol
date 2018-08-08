6d42b91e100e94a514bcf03541e0f20cbbf6648d
ReactARDev <talentmobile9999@gamil.com>
2018-08-08 03:26:32 -0700

    2. Transfer function added

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -1,12 +1,36 @@
 pragma solidity ^0.4.23;
 
 contract DappToken {
-	// Constructor
-	// Set the total number of tokens
-	// Read the total number of tokens
+
+	string public name = "DApp Token";
+	string public symbol = "DAPP";
+	string public standard = "DApp Token v1.0";
 	uint256 public totalSupply;
-	
-	function DappToken () public {
-		totalSupply = 1000000;
+
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value );
+
+	mapping(address => uint256) public balanceOf;
+
+	function DappToken (uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
 	}
+
+	// Transfer 
+	
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		// Exception if account doesn't have enough
+		require(balanceOf[msg.sender] >= _value);
+		// Transfer the balance
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		// Transfer Event		
+		Transfer(msg.sender, _to, _value);
+		// Return a boolem
+		return true;
+	}
+
 }
