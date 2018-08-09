9ba4c0670aa3c4c6494bbd71061840290e59c8f3
ReactARDev <talentmobile9999@gamil.com>
2018-08-09 01:09:27 -0700

    3. Delegate Transfer

--- token_sale/contracts/DappToken.sol
+++ token_sale/contracts/DappToken.sol
@@ -7,20 +7,32 @@ contract DappToken {
 	string public standard = "DApp Token v1.0";
 	uint256 public totalSupply;
 
+	// transfer event
 	event Transfer(
 		address indexed _from,
 		address indexed _to,
-		uint256 _value );
+		uint256 _value
+	);
 
+	// approve event
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
+
+	// balanceOf
 	mapping(address => uint256) public balanceOf;
-
+	// allow
+	mapping(address => mapping(address => uint256)) public allowance;
+	
+	// constructor
 	function DappToken (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
 		totalSupply = _initialSupply;
 	}
 
-	// Transfer 
-	
+	// transfer function
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		// Exception if account doesn't have enough
 		require(balanceOf[msg.sender] >= _value);
@@ -30,6 +42,33 @@ contract DappToken {
 		// Transfer Event		
 		Transfer(msg.sender, _to, _value);
 		// Return a boolem
+		return true;
+	}
+
+	// approve function
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		// allowance
+		allowance[msg.sender][_spender] = _value;	
+		// Approve event
+		Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+
+	//trnasferFrom function
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		// Require _from has enough Token
+		require(_value <= balanceOf[_from]);
+		// Require allowance is big enough
+		require(_value <= allowance[_from][msg.sender]);
+		// Change the balance
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		// Update the allowance
+		allowance[_from][msg.sender] -= _value;
+		// Transfer event
+		Transfer(_from, _to, _value);
+		// return a boolem
 		return true;
 	}
 
